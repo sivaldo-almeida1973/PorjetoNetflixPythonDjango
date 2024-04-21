b5b802901b8ae698ea3b268b75ec4f7a4646e581
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-04-21 19:33:02 -0300

    criando uma view para cada filme

--- filme/views.py
+++ filme/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Filme
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView , DetailView
 
 
 # Create your views here.-----------------------------------
@@ -25,6 +25,10 @@ class Homepage(TemplateView):
 class Homefilmes(ListView):#vai exibir lista de filmes do banco dados
     template_name = "homefilmes.html"
     model = Filme
-    # object_list
+    # object_list ->lista de itens do modelo
 
-
+#classe que vai mostrar os detalhes dos filmes
+class Detalhesfilme(DetailView):
+    template_name = "detalhesfilme.html"
+    model = Filme
+    # objec -> 1 item do nosso modelo
