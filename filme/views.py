7c191a91d84b21ea52e9d611f8ba8a26ddb569c6
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-04-22 12:48:55 -0300

    Vídeos em Tela Cheia e Listando Episódios no HTML

--- filme/views.py
+++ filme/views.py
@@ -4,23 +4,8 @@ from django.views.generic import TemplateView, ListView , DetailView
 
 
 # Create your views here.-----------------------------------
-#def homepage(request):     #template
-#  return render(request, "homepage.html")
-
-#essa classe substitui a função acima------------------------------
 class Homepage(TemplateView):
     template_name = "homepage.html"
-
-
-
-#sempre que for criar uma view, estamos criando uma
-# pagina nova para o site, entao temos que criar uma :
-# url - view - html
-#def homefilmes(request): #funcao homefilmes---------------------
-  #  context = {}
- #   lista_filmes = Filme.objects.all()
-#    context['lista_filmes'] = lista_filmes
-#    return render(request, "homefilmes.html", context)
 
 class Homefilmes(ListView):#vai exibir lista de filmes do banco dados
     template_name = "homefilmes.html"
@@ -32,3 +17,15 @@ class Detalhesfilme(DetailView):
     template_name = "detalhesfilme.html"
     model = Filme
     # objec -> 1 item do nosso modelo
+
+
+#def homepage(request):     #template
+#  return render(request, "homepage.html")
+
+
+# url - view - html
+#def homefilmes(request): #funcao homefilmes---------------------
+#    context = {}
+#    lista_filmes = Filme.objects.all()
+#    context['lista_filmes'] = lista_filmes
+#    return render(request, "homefilmes.html", context)
