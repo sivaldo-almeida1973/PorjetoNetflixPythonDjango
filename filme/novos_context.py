cc88e0c37c43df494872611842190834860826cc
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-04-27 22:51:12 -0300

    Edição da Página de homefilmes

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -2,12 +2,12 @@ from .models import Filme
 
 #lista de filmes recentes
 def lista_filmes_recentes(request):
-     lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:10]
+     lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:8]  #regula a qtde de filmes na secao filmes recentes
      return {"lista_filmes_recentes": lista_filmes}
 
 #lista de filmes em alta:
 def lista_filmes_emalta(request):
-      lista_filmes = Filme.objects.all().order_by('-visualizacoes')[0:10]
+      lista_filmes = Filme.objects.all().order_by('-visualizacoes')[0:8] #regula a qtde de filmes na secao filmes em alta
       return {"lista_filmes_emalta": lista_filmes}
 
 
