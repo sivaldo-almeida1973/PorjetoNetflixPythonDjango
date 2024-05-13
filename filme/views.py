9b4d9c55690582487476b50682436dad978865b0
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-05-12 21:09:29 -0300

    Personalizando a barra de navegação para usuários

--- filme/views.py
+++ filme/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Filme
 from django.views.generic import TemplateView, ListView , DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -6,6 +6,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.-----------------------------------
 class Homepage(TemplateView):
     template_name = "homepage.html"
+
+    def get(self, request, *args, **kwargs):
+         if request.user.is_authenticated:
+           # redireciona para s homefilmes
+           return redirect('filme:homefilmes')
+         else:
+             return super().get(request, *args, **kwargs)  #redireciona para a homepage
+
 
 class Homefilmes(LoginRequiredMixin, ListView):#vai exibir lista de filmes do banco dados
     template_name = "homefilmes.html"
